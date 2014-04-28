ba394b23025bab9d97e9a0d1d0e63e5004d6cf1b
wps5 <wps5@uw.edu>
2014-04-28 13:55:11 -0700

    Check if jquery is included in formulize.js

--- modules/formulize/libraries/formulize.js
+++ modules/formulize/libraries/formulize.js
@@ -25,3 +25,34 @@ jQuery(document).ready(function() {
         formulizechanged = 1;
     });
 });
+
+// check for jquery
+if (typeof jQuery == 'undefined' || jQuery.fn.jquery != "1.11.0") { 
+    // if jquery is not included or out of date
+    var head = document.getElementsByTagName('head')[0];
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+    head.appendChild(script);
+}
+
+// check for jquery migrate
+if (typeof jQuery.migrateWarnings == 'undefined') {
+    var jqmig = document.createElement('script');
+    jqmig.type = 'text/javascript';
+    jqmig.src = 'http://code.jquery.com/jquery-migrate-1.2.1.min.js';
+    document.getElementsByTagName('head')[0].appendChild(jqmig);
+}
+
+// check for jquery ui 
+if (!jQuery.ui || jQuery.ui.version != "1.8.2") {
+    var jqui = document.createElement('script');
+    jqui.type = 'text/javascript';
+    jqui.src = "".XOOPS_URL."/modules/formulize/libraries/jquery/jquery-ui-1.8.2.custom.min.js";
+    document.getElementsByTagName('head')[0].appendChild(jqui);
+
+    jquicss = document.createElement("link");
+    jquicss.type = 'test/css';
+    jquicss.href = "".XOOPS_URL."/modules/formulize/libraries/jquery/css/start/jquery-ui-1.8.2.custom.css";
+    document.getElementsByTagName('head')[0].appendChild(jquicss);
+}
